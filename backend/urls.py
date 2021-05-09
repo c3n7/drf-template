1d529ea11f968b466486591c03d9ff76c4828f67
Timothy Karani <kartimothy@gmail.com>
2021-05-10 01:09:18 +0300

    Added drf

--- backend/urls.py
+++ backend/urls.py
@@ -15,6 +15,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Dhurabini API",
+        default_version="v1",
+        description="Dhurabini HUdhibiti Rasilimali BIasharaNI API",
+        terms_of_service="https://github.com/c3n7/dhurabini_client/blob/main/license.txt",
+        contact=openapi.Contact(email="kartimothy@gmail.com"),
+        license=openapi.License(name="BSD 3-Clause License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,4 +38,8 @@ urlpatterns = [
     path('api/v1/dj-rest-auth/', include('dj_rest_auth.urls')),
     path('api/v1/dj-rest-auth/registration/',
          include('dj_rest_auth.registration.urls')),
+    path('swagger/', schema_view.with_ui(
+        'swagger', cache_timeout=0), name="schema-swagger-ui"),
+    path('redoc/', schema_view.with_ui(
+        'redoc', cache_timeout=0), name="schema-redoc"),
 ]
